072c887581c74534dda04bcc99479f351e99de7e
bk-schwemmer <b.k.schwemmer@me.com>
2022-11-22 01:49:34 -0800

    Added search functionality to Transactions screen. Ability to search transaction amount or payee for specific text enabled.

--- app/src/main/java/com/c196/bs_personal_finance/UI/ExpandableListAdapter.java
+++ app/src/main/java/com/c196/bs_personal_finance/UI/ExpandableListAdapter.java
@@ -2,6 +2,7 @@ package com.c196.bs_personal_finance.UI;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,48 +11,55 @@ import android.widget.TextView;
 
 import com.c196.bs_personal_finance.R;
 
-import java.text.DecimalFormat;
-import java.text.Format;
 import java.text.NumberFormat;
-import java.util.Formatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
+import java.util.Map;
 
 // Followed Tutorial at https://www.loginworks.com/blogs/work-expandablelistview-android/
 
 public class ExpandableListAdapter extends BaseExpandableListAdapter {
     private Context context;
-    private List<String> expandableListTitle;
-    private HashMap<String, List<String>> expandableListDetail;
+    private List<String> dateList;
+    private List<String> originalDateList;
+    private HashMap<String, List<String>> transactionsList;
+    private HashMap<String, List<String>> originalTransactionsList;
 
     public ExpandableListAdapter(Context context, List<String> expandableListTitle, HashMap<String, List<String>> expandableListDetail) {
         this.context = context;
-        this.expandableListTitle = expandableListTitle;
-        this.expandableListDetail = expandableListDetail;
+        this.originalDateList = new ArrayList<>();
+        this.originalDateList.addAll(expandableListTitle);
+        this.dateList = new ArrayList<>();
+        this.dateList.addAll(expandableListTitle);
+
+        this.originalTransactionsList = new HashMap<>();
+        this.originalTransactionsList.putAll(expandableListDetail);
+        this.transactionsList = new HashMap<>();
+        this.transactionsList.putAll(expandableListDetail);
     }
 
     @Override
     public int getGroupCount() {
-        return this.expandableListTitle.size();
+        return this.dateList.size();
     }
 
     @Override
     public int getChildrenCount(int listPosition) {
-        return this.expandableListDetail
-                .get(this.expandableListTitle.get(listPosition))
+        return this.transactionsList
+                .get(this.dateList.get(listPosition))
                 .size();
     }
 
     @Override
     public Object getGroup(int listPosition) {
-        return this.expandableListTitle.get(listPosition);
+        return this.dateList.get(listPosition);
     }
 
     @Override
     public Object getChild(int listPosition, int expandedListPosition) {
-        return this.expandableListDetail
-                .get(this.expandableListTitle
+        return this.transactionsList
+                .get(this.dateList
                 .get(listPosition))
                 .get(expandedListPosition);
     }
@@ -86,29 +94,29 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getChildView(int listPosition, final int expandedListPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        final String expandedListText = (String) getChild(listPosition, expandedListPosition);
+        final String transactionText = (String) getChild(listPosition, expandedListPosition);
         if (convertView == null) {
             LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = layoutInflater.inflate(R.layout.expandable_list_child, null);
         }
 
-        TextView expandedListChildTitleView = convertView.findViewById(R.id.childTitle);
-        TextView expandedListChildAmountView = convertView.findViewById(R.id.childAmount);
+        TextView transactionTitleView = convertView.findViewById(R.id.childTitle);
+        TextView transactionAmountView = convertView.findViewById(R.id.childAmount);
 
-        String[] parsedChild = expandedListText.split("_");
+        String[] parsedTransaction = transactionText.split("_");
 
-        expandedListChildTitleView.setText(parsedChild[1]);
-        if (parsedChild.length > 2) {
+        transactionTitleView.setText(parsedTransaction[1]);
+        if (parsedTransaction.length > 2) {
             NumberFormat nf = NumberFormat.getCurrencyInstance();
 
-            String amountString = parsedChild[2];
+            String amountString = parsedTransaction[2];
 
             double amount;
             if (TransactionDetails.validDouble(amountString)) {
                 amount = Double.parseDouble(amountString);
                 amountString = nf.format(amount);
             }
-            expandedListChildAmountView.setText(amountString);
+            transactionAmountView.setText(amountString);
         }
 
         return convertView;
@@ -117,5 +125,39 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     @Override
     public boolean isChildSelectable(int listPosition, int expandedListPosition) {
         return true;
+    }
+
+    public void filterData(String query) {
+        query = query.toLowerCase();
+        Log.v("TransactionListAdapter", String.valueOf(transactionsList.size()));
+        dateList.clear();
+        transactionsList.clear();
+        Log.v("TransactionListAdapter", String.valueOf(transactionsList.size()));
+
+        if (query.isEmpty()) {
+            dateList.addAll(originalDateList);
+            transactionsList.putAll(originalTransactionsList);
+            Log.v("TransactionListAdapter", String.valueOf(originalTransactionsList.size()));
+            Log.v("TransactionListAdapter", String.valueOf(transactionsList.size()));
+        } else {
+            for (Map.Entry<String, List<String>> transactionEntry : originalTransactionsList.entrySet()) {
+                List<String> transactions = new ArrayList<>();
+                for (String value : transactionEntry.getValue()) {
+                    String[] transactionData = value.split("_", 2);
+                    if (transactionData.length > 1) {
+                        if ((transactionData[1].toLowerCase()).contains(query)) {
+                            transactions.add(value);
+                            Log.v("Transaction Value", value);
+                        }
+                    }
+                }
+                if (transactions.size() > 0) {
+                    dateList.add(transactionEntry.getKey());
+                    transactionsList.put(transactionEntry.getKey(), transactions);
+                }
+            }
+        }
+        Log.v("TransactionListAdapter", String.valueOf(transactionsList.size()));
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/c196/bs_personal_finance/UI/Transactions.java
+++ app/src/main/java/com/c196/bs_personal_finance/UI/Transactions.java
@@ -25,22 +25,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Transactions extends AppCompatActivity {
+public class Transactions extends AppCompatActivity implements SearchView.OnQueryTextListener, SearchView.OnCloseListener{
 
     public static final String SELECTED_TRANSACTION_ID = "selectedTransactionID";
     public static final String TRANSACTION_PURPOSE = "transactionPurpose";
 
     private Repository repo;
-    private ExpandableListView expandableListView;
+    private ExpandableListView expandableList;
     private ExpandableListAdapter expandableListAdapter;
-    private List<String> expandableListTitle;
-    HashMap<String, List<String>> expandableListDetail;
+    List<Transaction> transactions;
+    private List<String> datesList;
+    HashMap<String, List<String>> transactionMap;
     private long currentUserID;
     private long currentAccountID = 0;
 
     private TextView noTransactionsView;
     private TextView transactionHeading;
-    private SearchView searchBar;
+    private SearchView search;
     private FloatingActionButton addTransactionButton;
 
     // CLICK LISTENERS
@@ -58,15 +59,48 @@ public class Transactions extends AppCompatActivity {
         startActivity(intent);
     };
 
+    private final ExpandableListView.OnChildClickListener transactionSelected =
+            ((parent, view, groupPosition, childPosition, id) -> {
+        Toast.makeText(
+                getApplicationContext(),
+                transactionMap.get(datesList.get(groupPosition)).get(childPosition),
+                Toast.LENGTH_LONG).show();
+
+        String[] parsedChild = transactionMap.get(datesList.get(groupPosition)).get(childPosition).split("_");
+        long transactionID = Long.parseLong(parsedChild[0]);
+        Intent intent = new Intent(Transactions.this, TransactionDetails.class);
+        intent.putExtra(SELECTED_TRANSACTION_ID, transactionID);
+        intent.putExtra(TRANSACTION_PURPOSE, getString(R.string.modify));
+        startActivity(intent);
+
+        return false;
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_transactions);
-        setTitle(getString(R.string.transactions));
 
         repo = new Repository(getApplication());
         fetchUiElements();
-        populateTransactionsList();
+        populatePageDetails();
+
+        // Set up search functionality
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        search.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        search.setIconifiedByDefault(false);
+        search.setOnQueryTextListener(this);
+        search.setOnCloseListener(this);
+
+        if (transactionsInAccount()) {
+            displayList();
+            expandAll();
+        } else {
+            noTransactionsView.setVisibility(View.VISIBLE);
+            expandableList.setVisibility(View.GONE);
+            transactionHeading.setVisibility(View.GONE);
+            search.setVisibility(View.GONE);
+        }
 
         addTransactionButton.setOnClickListener(addTransaction);
     }
@@ -74,12 +108,6 @@ public class Transactions extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_transactions, menu);
-
-//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//        SearchView searchView = (SearchView) menu.findItem(R.id.transactionSearch).getActionView();
-
-
-
         return true;
     }
 
@@ -109,116 +137,106 @@ public class Transactions extends AppCompatActivity {
     }
 
     private void fetchUiElements() {
-        expandableListView = findViewById(R.id.transactionsExpandableList);
+        expandableList = findViewById(R.id.transactionsExpandableList);
         addTransactionButton = findViewById(R.id.addTransactionButton);
         noTransactionsView = findViewById(R.id.noTransactions);
-        searchBar = findViewById(R.id.transactionSearch);
+        search = (SearchView) findViewById(R.id.transactionSearch);
         transactionHeading = findViewById(R.id.transactionHeading);
     }
 
-    private void populateTransactionsList() {
+    private void populatePageDetails() {
         currentAccountID = getIntent().getLongExtra(Accounts.CURRENT_ACCOUNT_ID, 0);
         currentUserID = repo.getAccountByID(currentAccountID).getUserID();
+
+        // Set page title
+        setTitle(getString(R.string.transactions));
+
+        // Set subtitle
         StringBuilder heading = new StringBuilder();
         heading.append(repo.getAccountByID(currentAccountID).getAccountName());
         heading.append(" ").append(getString(R.string.account));
         transactionHeading.setText(heading);
-
-        createTransactionHashMap();
-        expandableListTitle = new ArrayList<>(expandableListDetail.keySet());
-
-        // TODO Fix this once I change the keys to be dates instead of strings
-        expandableListTitle.sort(Collections.reverseOrder());
-        expandableListAdapter = new ExpandableListAdapter(this, expandableListTitle, expandableListDetail);
-        expandableListView.setAdapter(expandableListAdapter);
-
-        // Expand all groups by default
-        for (int i = 0; i < expandableListAdapter.getGroupCount(); i++) {
-            expandableListView.expandGroup(i);
-        }
-
-        expandableListView.setOnGroupExpandListener(groupPosition ->
-                Toast.makeText(getApplicationContext(),
-                expandableListTitle.get(groupPosition) + " List Expanded.",
-                Toast.LENGTH_SHORT).show());
-
-        expandableListView.setOnGroupCollapseListener(groupPosition ->
-                Toast.makeText(getApplicationContext(),
-                        expandableListTitle.get(groupPosition) + " List Collapsed.",
-                        Toast.LENGTH_SHORT).show());
-
-        expandableListView.setOnChildClickListener((parent, view, groupPosition, childPosition, id) -> {
-            Toast.makeText(
-                    getApplicationContext(),
-                    expandableListDetail.get(expandableListTitle.get(groupPosition)).get(childPosition),
-                    Toast.LENGTH_LONG).show();
-
-            String[] parsedChild = expandableListDetail.get(expandableListTitle.get(groupPosition)).get(childPosition).split("_");
-            long transactionID = Long.parseLong(parsedChild[0]);
-            Intent intent = new Intent(Transactions.this, TransactionDetails.class);
-            intent.putExtra(SELECTED_TRANSACTION_ID, transactionID);
-            intent.putExtra(TRANSACTION_PURPOSE, getString(R.string.modify));
-            startActivity(intent);
-
-            return false;
-        });
     }
 
-    private void createTransactionHashMap() {
-        expandableListDetail = new HashMap<>();
-        List<Transaction> transactions;
-        List<String> transactionDetails = new ArrayList<>();
+    private boolean transactionsInAccount() {
+        transactionMap = new HashMap<>();
         if (currentAccountID == 0) {
             transactions = repo.getTransactionsByUser(currentUserID);
         } else {
             transactions = repo.getTransactionByAccount(currentAccountID);
         }
 
-        if (transactions.size() == 0) {
-            noTransactionsView.setVisibility(View.VISIBLE);
-            expandableListView.setVisibility(View.GONE);
-            transactionHeading.setVisibility(View.GONE);
-            return;
+        return transactions.size() != 0;
+    }
+
+    private void expandAll() {
+        int count = expandableListAdapter.getGroupCount();
+        for (int i = 0; i < count; i++) {
+            expandableList.expandGroup(i);
         }
+    }
 
+    private void displayList() {
+        createList();
+        expandableListAdapter = new ExpandableListAdapter(this, datesList, transactionMap);
+        expandableList.setAdapter(expandableListAdapter);
 
-        // TODO DELETE THIS
-        int i = 1;
-        for (Transaction transaction : transactions) {
-            System.out.println(i + ": " + transaction.getTransactionID() + " | " +
-                    transaction.getPayee() + " | " +
-                    transaction.getAmount() + " | " +
-                    transaction.getCategory());
-            i++;
-        }
+        expandableList.setOnGroupExpandListener(groupPosition ->
+                Toast.makeText(getApplicationContext(),
+                        datesList.get(groupPosition) + " List Expanded.",
+                        Toast.LENGTH_SHORT).show());
 
+        expandableList.setOnGroupCollapseListener(groupPosition ->
+                Toast.makeText(getApplicationContext(),
+                        datesList.get(groupPosition) + " List Collapsed.",
+                        Toast.LENGTH_SHORT).show());
+
+        expandableList.setOnChildClickListener(transactionSelected);
+    }
+
+    private void createList() {
+
+        List<String> transactionDetails = new ArrayList<>();
         int counter = 0;
         String transactionDate = transactions.get(0).getDate();
         for (Transaction transaction : transactions) {
             counter++;
 
-            // TODO DELETE
-            System.out.println("TransactionDate = " + transactionDate);
-            System.out.println("CurrentDate = " + transaction.getDate());
-
-            // IF the current date doesn't match the previous date, submit the list to the hashmap then start the list over
+            // If the current date doesn't match the previous date, submit the list to the hashmap then start the list over
             if (!transaction.getDate().equals(transactionDate)) {
-                expandableListDetail.put(transactionDate, transactionDetails);
+                transactionMap.put(transactionDate, transactionDetails);
                 transactionDate = transaction.getDate();
                 transactionDetails = new ArrayList<>();
             }
             transactionDetails.add(transaction.getTransactionID() + "_" + transaction.getPayee() + "_" + transaction.getAmount());
 
             if (counter == transactions.size()) {
-                expandableListDetail.put(transactionDate, transactionDetails);
+                transactionMap.put(transactionDate, transactionDetails);
             }
         }
 
-        // TODO DELETE
-        int index = 1;
-        for (Map.Entry<String, List<String>> entry: expandableListDetail.entrySet()) {
-            System.out.println("#" + index + ": " + entry.getKey() + " | " + entry.getValue());
-            index++;
-        }
+        datesList = new ArrayList<>(transactionMap.keySet());
+        datesList.sort(Collections.reverseOrder());
+    }
+
+    @Override
+    public boolean onClose() {
+        expandableListAdapter.filterData("");
+        expandAll();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String newText) {
+        expandableListAdapter.filterData(newText);
+        expandAll();
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+        expandableListAdapter.filterData(query);
+        expandAll();
+        return false;
     }
 }
